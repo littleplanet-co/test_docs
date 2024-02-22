b9f42c2600c196d8e223787b2be7858a261e8281
hanjin.kwak <ko.dingcoo@gmail.com>
2024-02-22 10:51:13 +0900

    feat: 페이지 구조 변경
    1. query parameter 형태로 문서를 찾도록 수정

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/littleplanet-co/test_docs/main/logme_service_back-end_openapi.json",
+    url: "https://raw.githubusercontent.com/littleplanet-co/test_docs/main/logme_premium-service_back-end_openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
