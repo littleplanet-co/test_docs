52be0541a84ab564a61e5220e1d0f25cdd80e4aa
hanjin.kwak <ko.dingcoo@gmail.com>
2024-02-22 10:59:29 +0900

    fix: url 경로 오류 수정 등
    1. swagger-initializer.js 경로 오류 수정
    2. 문서 추가

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,9 +1,15 @@
+
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
+  var query = location.search.split(/[?&]/)[1];
+  var fileName = query.split('=')[1];
+  console.log(fileName);
+
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/littleplanet-co/test_docs/main/logme_premium-service_back-end_openapi.json",
+    // url: "https://raw.githubusercontent.com/littleplanet-co/test_docs/main/logme_service_back-end_openapi.json",
+    url: "https://raw.githubusercontent.com/littleplanet-co/test_docs/main/docs/" + fileName + ".json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
